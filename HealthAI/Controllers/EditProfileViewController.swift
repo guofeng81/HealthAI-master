9ed621c7318e953d53f3157de08ee5d49e9a6d87
Feng Guo <fguo81@gmail.com>
2018-10-23 11:31:58 -0500

    Complete Navigation Drawer for editing the profile and sign out features. The main page still need to fix the layout.

---
+++ HealthAI/Controllers/EditProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditProfileViewController.swift
+//  HealthAI
+//
+//  Created by Feng Guo on 10/23/18.
+//  Copyright © 2018 Team9. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
--- HealthAI/Controllers/HealthMainViewController.swift
+++ HealthAI/Controllers/HealthMainViewController.swift
@@ -37,26 +37,29 @@ class HealthMainViewController: UIViewController, CLLocationManagerDelegate, UIN
     let weatherDataModel = WeatherDataModel()
     let locationManager = CLLocationManager()
     
+    var sidebarView: SidebarView!
+    var blackScreen : UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //MARK - Add CardView Gesture
-        let TapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
-        self.workoutCardView.addGestureRecognizer(TapGesture)
+        //let TapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        //self.workoutCardView.addGestureRecognizer(TapGesture)
         
         loadLocationManager()
         
-        if let user =  Auth.auth().currentUser {
-            print(user.email ?? "No email address!")
-            print(user.uid)
-        }        
+        setupMenu()
+        
+//        if let user =  Auth.auth().currentUser {
+//            print(user.email ?? "No email address!")
+//            print(user.uid)
+//        }
     }
     
     @objc func handleTap(sender:UITapGestureRecognizer){
         performSegue(withIdentifier: "goToWorkout", sender: self)
     }
-    
-    
     
     
     @IBAction func cameraPressed(_ sender: UIButton) {
@@ -144,7 +147,7 @@ class HealthMainViewController: UIViewController, CLLocationManagerDelegate, UIN
             weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
             weatherDataModel.weatherCondition = json["weather"]["main"].stringValue
             
-            updateUIWeatherData(weatherDataModel: weatherDataModel)
+            //updateUIWeatherData(weatherDataModel: weatherDataModel)
             
             print(weatherDataModel.temperature)
             print(weatherDataModel.city)
@@ -162,7 +165,7 @@ class HealthMainViewController: UIViewController, CLLocationManagerDelegate, UIN
     
     func updateUIWeatherData(weatherDataModel: WeatherDataModel){
     
-        cityLabel.text = weatherDataModel.city
+        //cityLabel.text = weatherDataModel.city
         temperatureLabel.text = "\(weatherDataModel.temperature)°C"
         weatherImage.image = UIImage(named: weatherDataModel.weatherIconName)
         //conditionLabel.text = weatherDataModel.weatherCondition
@@ -183,4 +186,76 @@ class HealthMainViewController: UIViewController, CLLocationManagerDelegate, UIN
         
     }
     
+    func setupMenu(){
+        
+        let btnMenu = UIBarButtonItem(image: #imageLiteral(resourceName: "menu"), style: .plain, target: self, action: #selector(btnMenuAction))
+        btnMenu.tintColor=UIColor(red: 54/255, green: 55/255, blue: 56/255, alpha: 1.0)
+        self.navigationItem.leftBarButtonItem = btnMenu
+        
+        sidebarView=SidebarView(frame: CGRect(x: 0, y: 0, width: 0, height: self.view.frame.height))
+        sidebarView.delegate = self
+        sidebarView.layer.zPosition=100
+        self.view.isUserInteractionEnabled=true
+        self.navigationController?.view.addSubview(sidebarView)
+        
+        blackScreen=UIView(frame: self.view.bounds)
+        blackScreen.backgroundColor=UIColor(white: 0, alpha: 0.5)
+        blackScreen.isHidden=true
+        self.navigationController?.view.addSubview(blackScreen)
+        blackScreen.layer.zPosition=99
+        let tapGestRecognizer = UITapGestureRecognizer(target: self, action: #selector(blackScreenTapAction(sender:)))
+        blackScreen.addGestureRecognizer(tapGestRecognizer)
+        
+    }
+    
+    @objc func btnMenuAction() {
+        blackScreen.isHidden=false
+        UIView.animate(withDuration: 0.3, animations: {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 250, height: self.sidebarView.frame.height)
+        }) { (complete) in
+            self.blackScreen.frame=CGRect(x: self.sidebarView.frame.width, y: 0, width: self.view.frame.width-self.sidebarView.frame.width, height: self.view.bounds.height+100)
+        }
+    }
+    
+    @objc func blackScreenTapAction(sender: UITapGestureRecognizer) {
+        blackScreen.isHidden=true
+        blackScreen.frame=self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
+    }
+    
 }
+
+extension HealthMainViewController: SidebarViewDelegate {
+    func sidebarDidSelectRow(row: Row) {
+        blackScreen.isHidden=true
+        blackScreen.frame=self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
+        switch row {
+        case .editProfile:
+            let vc=EditProfileViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        case .messages:
+            print("Messages")
+        case .contact:
+            print("Contact")
+        case .settings:
+            print("Settings")
+        case .history:
+            print("History")
+        case .help:
+            print("Help")
+        case .signOut:
+            print("Sign out")
+        case .none:
+            break
+        }
+    }
+}
+
+
+
+
